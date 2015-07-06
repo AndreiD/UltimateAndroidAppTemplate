f29b6f8cb1fdd2c8aea33852f82c3fc914830cb0
AndreiD <danandreist@gmail.com>
2015-07-06 17:07:15 +0300

    forgot to add mybaseactivity

---
+++ app/src/main/java/com/andrei/template/MyBaseActivity.java
@@ -0,0 +1,20 @@
+package com.andrei.template;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import roboguice.activity.RoboActivity;
+
+
+public class MyBaseActivity extends RoboActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+
+}
